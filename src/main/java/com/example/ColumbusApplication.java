bec047617ebe91f3bddbb0581ac2a6efaf088764
HikaruMiyahara <hikaru@HikaruMiyaharanoMacBook-Pro.local>
2017-11-01 08:11:48 +0900

    Add userMenuController

--- src/main/java/com/example/ColumbusApplication.java
+++ src/main/java/com/example/ColumbusApplication.java
@@ -1,12 +1,15 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ColumbusApplication {
-
+public class ColumbusApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ColumbusApplication.class, args);
 	}
+	
 }
